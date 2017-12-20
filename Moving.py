cd79917a53101630e3d7e13bfbd8d53d8af9cde0
Bugzee <Bugzee321@users.noreply.github.com>
2017-12-20 17:28:20 +0200

    Add files via upload

--- Moving.py
+++ Moving.py
@@ -1,75 +1,109 @@
 import numpy as np
 
 #if it is a valid places to any player it will return all states and its sucessors it will be earned else it will return null
-def decide_place(self , color , board):
+def decide_place(color , board):
     validMoves = {}
     validParts = {}
     valid = 0
     for x in range(8):
         for y in range(8):
             place = [x , y]
-            income = self.isValidMove(board, color, place)
-            if income != False:
-                validMoves[valid] = place
+
+            income = isv(board, color, place)
+
+            if len(income) > 0:
+                #print  place
+                #validMoves[valid] = place
                 validParts[valid] = income
                 valid += 1
     return validMoves , validParts , valid
 
     #detrmine weather the place is on table or not
-def isOntabel(self, place):
+def isOntabel(place):
     return place[0] >= 0 and place[0] <= 7 and place[1] >= 0 and place[1] <= 7
 
     #detrimne if the move is valid or not and if it is vlid it returns the places which will be turned else it will return false
-def isValidMove(self, board , color , place):
-    validMoves = []
-    # if board[place[0]][place[1]].Color != 'E' or self.isOntabel(place) == False:
-    #     return 'invalid place'
+# def isValidMove(board , color , place):
+#     validMoves = []
+#
+#     if color == 'W':
+#         othercolor = 'B'
+#     else:
+#         othercolor = 'W'
+#     copyOfPlace = place
+#
+#     for x, y in [[1, 0], [-1, 0], [-1, -1], [0, -1], [1, -1], [-1, 1], [0, 1], [1, 1]]:
+#         xstart, ystart = x , y
+#         copyOfPlace[0] = copyOfPlace[0] + x
+#         copyOfPlace[1] = copyOfPlace[1] + y
+#         if not isOntabel(copyOfPlace):
+#             continue
+#         # There is a other color here that may be earned
+#         if board[copyOfPlace[0]][copyOfPlace[1]].Color == othercolor:
+#             copyOfPlace[0] = copyOfPlace[0] + x
+#             copyOfPlace[1] = copyOfPlace[1] + y
+#             if not isOntabel(copyOfPlace):
+#                 continue
+#             while board[copyOfPlace[0]][copyOfPlace[1]].Color == othercolor:
+#
+#                 copyOfPlace[0] = copyOfPlace[0] + x
+#                 copyOfPlace[1] = copyOfPlace[1] + y
+#                 #print copyOfPlace
+#
+#                 if not isOntabel(place):
+#                     continue
+#             if not isOntabel(copyOfPlace):
+#                 continue
+#             #There is a other color here that will be earned
+#             if board[copyOfPlace[0]][copyOfPlace[1]].Color == color:
+#
+#                 while True:
+#                     copyOfPlace[0] = copyOfPlace[0] - x
+#                     copyOfPlace[1] = copyOfPlace[1] - y
+#                     if x == xstart and y == ystart:
+#                         break
+#                     validMoves.append(copyOfPlace)
+#     return validMoves
+
+def isv(board , color , place):
+    valid = []
     if color == 'W':
         othercolor = 'B'
     else:
         othercolor = 'W'
 
-    copyOfPlace = place
+    copyOfPlace = list(place)
     for x, y in [[1, 0], [-1, 0], [-1, -1], [0, -1], [1, -1], [-1, 1], [0, 1], [1, 1]]:
-        xstart, ystart = x, y
+        xstart, ystart = x , y
         copyOfPlace[0] = copyOfPlace[0] + x
         copyOfPlace[1] = copyOfPlace[1] + y
-
-        # There is a other color here that may be earned
-        if self.isOntabel(copyOfPlace) and board[copyOfPlace[0]][copyOfPlace[1]].Color == othercolor:
+        #print copyOfPlace
+        if not isOntabel(copyOfPlace):
+            continue
+        while board[copyOfPlace[0]][copyOfPlace[1]].Color == othercolor:
             copyOfPlace[0] = copyOfPlace[0] + x
             copyOfPlace[1] = copyOfPlace[1] + y
-            if not self.isOntabel(copyOfPlace):
-                continue
-            while board[copyOfPlace[0]][copyOfPlace[1]].Color == othercolor:
-                print copyOfPlace
-                copyOfPlace[0] = copyOfPlace[0] + x
-                copyOfPlace[1] = copyOfPlace[1] + y
-                if not self.isOntabel(place):
-                    break
-            if not self.isOntabel(copyOfPlace):
-                continue
-            # There is a other color here that will be earned
-            if board[copyOfPlace[0]][copyOfPlace[1]].Color == color:
-                while True:
-                    copyOfPlace[0] = copyOfPlace[0] - x
-                    copyOfPlace[1] = copyOfPlace[1] - y
-                    if x == xstart and y == ystart:
-                        break
-                    validMoves.append(copyOfPlace)
-    if len(validMoves) == 0:
-        return False
-    return validMoves
+            print place ,  copyOfPlace
+
+        if board[copyOfPlace[0]][copyOfPlace[1]].Color == color:
+            while x >= xstart and y >= ystart:
+                copyOfPlace[0] = copyOfPlace[0] - x
+                copyOfPlace[1] = copyOfPlace[1] - y
+                valid.append(list(copyOfPlace))
+
+        copyOfPlace = list(place)
+    return valid
+
 
 #return state of states
-def Minimax(self, color, board):
-    validList = self.decide_place(color , board)[0]
+def Minimax(color, board):
+    validList = decide_place(color , board)[0]
     valid_List = validList.values()
     theminimax_values = []
 
     for i in valid_List:
         score = []
-        the_highest_score = self.getComputerPlay(board, color, i, score, 0)
+        the_highest_score = getComputerPlay(board, color, i, score, 0)
         theminimax_values.append([i, the_highest_score])
     maxx = -100
 
@@ -133,7 +167,7 @@ def get_all_weighted_score(board):
                 black += 1
     return white , black
 
-def getScore(self , board):
+def getScore(board):
     black , white = get_all_weighted_score(board)
     return black - white
 
